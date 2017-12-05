39d470871f231c1bf3f702527b7f6cc3c04bd2af
Nhat Hoang <nhat.hoang@apps-cyclone.com>
2017-12-05 21:14:54 +0700

    + Update thiet bi phan phoi khi

--- AerotankCalculation/app/src/main/java/com/example/training/acc/aerotankcalculation/fragment/ThietBiPhanPhoiKhiFragment.java
+++ AerotankCalculation/app/src/main/java/com/example/training/acc/aerotankcalculation/fragment/ThietBiPhanPhoiKhiFragment.java
@@ -8,16 +8,78 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.training.acc.aerotankcalculation.R;
 import com.example.training.acc.aerotankcalculation.base.BaseFragment;
+import com.example.training.acc.aerotankcalculation.utils.AppUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class ThietBiPhanPhoiKhiFragment extends BaseFragment {
+    @BindView(R.id.fragPPK_etA)
+    EditText etA;
+    @BindView(R.id.fragPPK_etBODVao)
+    EditText etBODVao;
+    @BindView(R.id.fragPPK_etH)
+    EditText etH;
+    @BindView(R.id.fragPPK_etQDia)
+    EditText etQDia;
+    @BindView(R.id.fragPPK_etQtb)
+    EditText etQtb;
+    @BindView(R.id.fragPPK_etBODRa)
+    EditText etBODRa;
+    @BindView(R.id.fragPPK_etN)
+    EditText etN;
+    @BindView(R.id.fragPPK_tvApLuc)
+    TextView tvApLuc;
+    @BindView(R.id.fragPPK_tvCongSuat)
+    TextView tvCongSuat;
+    @BindView(R.id.fragPPK_tvKhongKhiCap)
+    TextView tvKhongKhiCap;
+    @BindView(R.id.fragPPK_tvOngDanChinh)
+    TextView tvOngDanChinh;
+    @BindView(R.id.fragPPK_tvOngDanNhanh)
+    TextView tvOngDanNhanh;
+    @BindView(R.id.fragPPK_tvSoDia)
+    TextView tvSoDia;
+
+    private double mLuongKhongKhiCungcap, mApLucCanThiet, mCongSuatMayThoiKhi, mSoDiaPhanPhoiKhi, mDuongKinhChinh, mDuongKinhNhanh;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_thiet_bi_phan_phoi_khi, container, false);
         setActionBarBack(view, getString(R.string.thiet_bi_phan_phoi_khi).toUpperCase());
+        ButterKnife.bind(this, view);
         return view;
+    }
+
+    @OnClick(R.id.fragPPK_tvKetQua)
+    public void onClick() {
+        double a = Double.parseDouble(etA.getText().toString());
+        double bodVao = Double.parseDouble(etBODVao.getText().toString());
+        double bodRa = Double.parseDouble(etBODRa.getText().toString());
+        double h = Double.parseDouble(etH.getText().toString());
+        double qDia = Double.parseDouble(etQDia.getText().toString());
+        double qTB = Double.parseDouble(etQtb.getText().toString());
+        double n = Double.parseDouble(etN.getText().toString());
+
+        double mLuongKhongKhiCungcap = AppUtils.luongKhongKhiCungcap(qTB, bodVao, bodRa, a, h);
+        double mApLucCanThiet = AppUtils.apLucCanThiet(h);
+        double mCongSuatMayThoiKhi = AppUtils.congSuatMayThoiKhi(mApLucCanThiet, mLuongKhongKhiCungcap);
+        double mSoDiaPhanPhoiKhi = AppUtils.soDiaPhanPhoiKhi(mLuongKhongKhiCungcap, qDia);
+        double mDuongKinhChinh = AppUtils.duongKinhChinh(mLuongKhongKhiCungcap);
+        double mDuongKinhNhanh = AppUtils.duongKinhNhanh(mLuongKhongKhiCungcap, n);
+
+        tvApLuc.setText(getText(R.string.kq_ap_luc_can_thiet) + " " + String.valueOf(mApLucCanThiet) + " m");
+        tvKhongKhiCap.setText(getText(R.string.kq_luong_kk) + " " + String.valueOf(mLuongKhongKhiCungcap) + " m³/s");
+        tvCongSuat.setText(getText(R.string.kq_cong_suat_may_thoi_khi) + " " + String.valueOf(mCongSuatMayThoiKhi) + " KW");
+        tvOngDanChinh.setText(getText(R.string.kq_duong_kinh) + " " + String.valueOf(mDuongKinhChinh) + " m");
+        tvOngDanNhanh.setText(getText(R.string.kq_duong_kinh_nhanh) + " " + String.valueOf(mDuongKinhNhanh) + " m");
+        tvSoDia.setText(getText(R.string.kq_so_dia_phan_phoi) + " " + String.valueOf(mSoDiaPhanPhoiKhi) + " đĩa");
     }
 }
--- AerotankCalculation/app/src/main/java/com/example/training/acc/aerotankcalculation/utils/AppUtils.java
+++ AerotankCalculation/app/src/main/java/com/example/training/acc/aerotankcalculation/utils/AppUtils.java
@@ -48,4 +48,28 @@ public class AppUtils {
     public static double luuLuongTuanHoan(double qTb, double x, double xt) {
         return Math.round(qTb * (x / (0.8 * xt - x)));
     }
+
+    public static double luongKhongKhiCungcap(double qTB, double bodVao, double bodRa, double a, double h) {
+        return ((double) Math.round(((((qTB * (bodVao - bodRa)) / (1000 * 0.68) - 1.42 * a) * 1.83) * 1500) / (7 * h * 86400) * 1000) / 1000);
+    }
+
+    public static double apLucCanThiet(double h) {
+        return h + 0.9;
+    }
+
+    public static double congSuatMayThoiKhi(double apLucCanThiet, double luongKhongKhi) {
+        return ((double) Math.round(((34400 * (Math.pow(((10.33 + apLucCanThiet) / 10.33), 0.29) - 1) * luongKhongKhi / (102 * 0.8)) * 10) / 10));
+    }
+
+    public static double soDiaPhanPhoiKhi(double luongKK, double qDia) {
+        return Math.round(luongKK * 3600 / qDia);
+    }
+
+    public static double duongKinhChinh(double luongKK) {
+        return ((double) Math.round((Math.sqrt((4 * luongKK) / (3.14 * 15))) * 1000) / 1000);
+    }
+
+    public static double duongKinhNhanh(double luongKK, double n) {
+        return ((double) Math.round((Math.sqrt((4 * luongKK) / (3.14 * 15 * n))) * 10000) / 10000);
+    }
 }
